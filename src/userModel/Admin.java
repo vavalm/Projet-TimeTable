c30d087054b99a91b5814fbaccf580eda91ce811
juflajollet@gmail.com <Gitpro123>
2016-05-20 15:14:06 +0200

    Admin + User classes Update

--- src/userModel/Admin.java
+++ src/userModel/Admin.java
@@ -1,5 +1,20 @@
 package userModel;
 
-public class Admin {
-
+public class Admin extends User {
+	
+	private int adminID;
+	
+	public Admin(String login, String firstname, String surname, String pwd, int adminID){
+		super(login, firstname, surname, pwd);
+		setAdminID(adminID);
+	}
+	
+	public void setAdminID(int adminID){
+		this.adminID = adminID;
+	}
+	
+	public int getAdminID(){
+		return this.adminID;
+	}
+	
 }
--- src/userModel/User.java
+++ src/userModel/User.java
@@ -1,5 +1,50 @@
 package userModel;
 
-public class User {
 
+
+public class User {
+	
+	private String login;
+	private String firstname;
+	private String surname;
+	private String pwd;
+	
+	public User (String login, String firstname, String surname, String pwd){
+		setLogin(login);
+		setFirstname(firstname);
+		setSurname(surname);
+		setPassword(pwd);
+	}
+	
+	public void setLogin(String login){
+		this.login = login;
+	}
+	
+	public String getLogin(){
+		return this.login;
+	}
+	
+	public void setFirstname(String firstname){
+		this.firstname = firstname;
+	}
+	
+	public String getFirstname(){
+		return this.firstname;
+	}
+	
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	
+	public String getSurname(){
+		return this.surname;
+	}
+	
+	public void setPassword(String pwd){
+		this.pwd = pwd;
+	}
+	
+	public String getPassword(){
+		return this.pwd;
+	}
 }
