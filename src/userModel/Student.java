0e4892f86902a872e866a556b1fd815073a64554
Andw4n <ydna.andr@gmail.com>
2016-05-21 13:33:30 +0200

    Implement Class Student & Teacher

--- src/userModel/Student.java
+++ src/userModel/Student.java
@@ -1,5 +1,19 @@
 package userModel;
 
-public class Student {
+public class Student extends User{
+    private int studentID;
+
+    public Student(String login, String firstname, String surname, String pwd, int studentID){
+        super(login, firstname, surname, pwd);
+        setStudentID(studentID);
+    }
+
+    public void setStudentID(int studentID){
+        this.studentID = studentID;
+    }
+
+    public int getStudentID(){
+        return this.studentID;
+    }
 
 }
--- src/userModel/Teacher.java
+++ src/userModel/Teacher.java
@@ -1,5 +1,19 @@
 package userModel;
 
-public class Teacher {
+public class Teacher extends User{
+    private int teacherID;
+
+    public Teacher(String login, String firstname, String surname, String pwd, int teacherID){
+        super(login, firstname, surname, pwd);
+        setTeacherID(teacherID);
+    }
+
+    public void setTeacherID(int teacherID){
+        this.teacherID = teacherID;
+    }
+
+    public int getTeacherID(){
+        return this.teacherID;
+    }
 
 }
