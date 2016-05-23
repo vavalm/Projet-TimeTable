3355d613e9ccd68feeca6ba30755ef3d6961a2d6
Andw4n <ydna.andr@gmail.com>
2016-05-23 22:17:39 +0200

    Implement class Group

--- src/userModel/Group.java
+++ src/userModel/Group.java
@@ -1,5 +1,43 @@
 package userModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private int groupId;
+    private int studentNumber;
+    private List<Student> composition;
+
+
+
+    public Group(int groupId, int studentNumber){
+        this.groupId = groupId;
+        this.studentNumber = studentNumber;
+        this.composition = new ArrayList<Student>();
+    }
+
+
+
+
+
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+
 
 }
